f17d7fa0c193e2774ab06c307589d680ababbfd3
Aleksandra Popova <vooxi@yandex.ru>
2021-11-01 17:48:05 +0300

    add main function and remove global variables

--- main.py
+++ main.py
@@ -3,26 +3,31 @@ import sys
 import re
 import tqdm
 
-path = sys.argv[1]
-word = sys.argv[2]
-file_for_write = open('result.txt', 'w')
-
-
-def checking(file, word, file_for_write):
-    with tqdm.tqdm(total=os.path.getsize(file)) as progress_bar:
-        with open(file, 'r') as file_for_read:
-            for line in file_for_read:
+def checkFileAndWrite(rfile_path, line_mask, wfile):
+    with tqdm.tqdm(total=os.path.getsize(rfile_path)) as progress_bar:
+        with open(rfile_path, 'r') as rfile:
+            for line in rfile:
                 progress_bar.update(len(line) + 1)
-                if re.search(word, line):
-                    file_for_write.write(line)
+                if re.search(line_mask, line):
+                    wfile.write(line)
 
 
-if os.path.isfile(path):
-    checking(path, word, file_for_write)
 
-if os.path.isdir(path):
-    for file in os.listdir(path):
-        if file.endswith(".log"):
-            checking(path + '\\' + file, word, file_for_write)
+def main():
+    path = sys.argv[1]
+    word = sys.argv[2]
+    result_file = open('result.txt', 'w')
 
-file_for_write.close()
+    if os.path.isfile(path):
+        checkFileAndWrite(path, word, result_file)
+
+    if os.path.isdir(path):
+        for file in os.listdir(path):
+            if file.endswith(".log"):
+                checkFileAndWrite(path + '\\' + file, word, result_file)
+
+    result_file.close()
+
+
+if __name__ == '__main__':
+    main()
