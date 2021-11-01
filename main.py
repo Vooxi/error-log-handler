84c4772b1d9ab1622b90ce0fd75646a3bd6dd6c1
Aleksandra Popova <vooxi@yandex.ru>
2021-11-01 17:50:07 +0300

    small refactoring

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 import re
 import tqdm
 
+FILE_RESULT_PATH = 'result.txt'
+
 def checkFileAndWrite(rfile_path, line_mask, wfile):
     with tqdm.tqdm(total=os.path.getsize(rfile_path)) as progress_bar:
         with open(rfile_path, 'r') as rfile:
@@ -12,21 +14,18 @@ def checkFileAndWrite(rfile_path, line_mask, wfile):
                     wfile.write(line)
 
 
-
 def main():
     path = sys.argv[1]
     word = sys.argv[2]
-    result_file = open('result.txt', 'w')
 
-    if os.path.isfile(path):
-        checkFileAndWrite(path, word, result_file)
+    with open(FILE_RESULT_PATH, 'w') as result_file:
+        if os.path.isfile(path):
+            checkFileAndWrite(path, word, result_file)
 
-    if os.path.isdir(path):
-        for file in os.listdir(path):
-            if file.endswith(".log"):
-                checkFileAndWrite(path + '\\' + file, word, result_file)
-
-    result_file.close()
+        if os.path.isdir(path):
+            for file in os.listdir(path):
+                if file.endswith(".log"):
+                    checkFileAndWrite(path + '\\' + file, word, result_file)
 
 
 if __name__ == '__main__':
